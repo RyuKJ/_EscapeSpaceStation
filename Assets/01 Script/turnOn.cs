c67a0ef9b9f682d82c22fa19e9d14238e6b8eb53
KJ-Ryu <kj@namu-ai.com>
2023-02-27 17:09:29 +0900

    샘플 룸 제작

---
+++ Assets/01 Script/turnOn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnOn : MonoBehaviour {
+
+    public GameObject cubeResult;
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == ("GrabObj"))
+        {
+            cubeResult.SetActive(true);
+            Destroy(coll.gameObject);
+        }
+
+    }
+
+}
